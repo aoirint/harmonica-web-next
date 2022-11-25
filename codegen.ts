fec49f6687d8cb1ebd3ba29e146c113527e5e152
aoirint <aoirint@gmail.com>
2022-11-25 10:07:34 +0900

    add codegen.ts

---
+++ codegen.ts
@@ -0,0 +1,23 @@
+
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: {
+    [`${process.env.HASURA_URL}/v1/graphql`]: {
+      headers: {
+        "X-Hasura-Admin-Secret": `${process.env.HASURA_ADMIN_SECRET}`
+      }
+    }
+  },
+  documents: "pages/**/*.tsx",
+  ignoreNoDocuments: true,
+  generates: {
+    "./generated/gql/": {
+      preset: "client",
+      plugins: []
+    }
+  }
+};
+
+export default config;
